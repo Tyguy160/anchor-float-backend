148d5a5d73494f7394a26231718e7b183cea3aff
Matt Meinzer <matt.meinzer@wheniwork.com>
2020-01-27 23:03:35 -0600

    Add userId as input field to report generation

--- report/cmd.js
+++ report/cmd.js
@@ -3,9 +3,10 @@ const uuid = require('uuid/v4');
 const { reportProducer } = require('./producers.js');
 
 const hostnameInput = process.argv.slice(2, 3)[0];
-const userId = 'test';
+const userId = process.argv.slice(3, 4)[0];
 
 console.log(`Generating a report for: ${hostnameInput}`);
+console.log({ hostnameInput, userId });
 
 const taskId = uuid();
 
